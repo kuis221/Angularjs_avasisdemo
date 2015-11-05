9dd426f30c7c4cc0b1fe516f8fce6115f65a8450
Val <valikhan.b@gmail.com>
2015-11-05 15:05:06 -0500

    heroku deployment

---
+++ server.js
@@ -0,0 +1,11 @@
+﻿var express = require("express");
+var app = express();
+
+app.use(express.logger());
+app.use(express.static('public'));
+
+var port = process.env.PORT || 5000;
+
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
