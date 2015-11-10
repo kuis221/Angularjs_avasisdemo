d3cd96b06f0c1fbb2dfdb6124022ea94ebaa3560
Val <valikhan.b@gmail.com>
2015-11-10 12:13:51 -0500

    fixes

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.use(express.static('www'));
 
 var port = process.env.PORT || 5000;
 
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function () {
+    console.log("Listening on " + port);
 });
