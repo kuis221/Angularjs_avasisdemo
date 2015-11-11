c6927692479ba0d5bde2e717567e98fd27834743
Val <valikhan.b@gmail.com>
2015-11-10 19:33:18 -0500

    server.js fix

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ var app = express();
 app.use(express.basicAuth('avasis', 'demo'));
 app.use(express.logger());
 app.use(express.static('www'));
-app.use('/app/', express.static('public'));
+app.use('/app', express.static('public'));
 app.use('/app/assets', express.static('www/assets'));
+app.use('/bower_components', express.static('public/bower_components'));
+app.use('/content', express.static('public/bower_components'));
+app.use('/partials', express.static('public/bower_components'));
 app.use(slashes());
 
 var port = process.env.PORT || 5000;
