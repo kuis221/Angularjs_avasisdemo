86579a0de396764d93bca10fb1be57de24ea4506
Manish Katoch <manish.katoch9@gmail.com>
2015-11-25 00:23:06 +0530

    Financials-Main

---
+++ public/partials/financials/financials-main.js
@@ -0,0 +1,21 @@
+app.controller('FinancialsCtrl',
+    ['$scope', '$stateParams', function ($scope, $stateParams) {
+        $scope.arcConfig = {
+            size: 200,
+            scale: 2,
+            arcs: [
+                {
+                    percent: 76,
+                    color: '#3aabd5',
+                    width: 15,
+                    radius: 70
+                },
+                {
+                    percent: 61,
+                    color: '#79c485',
+                    width: 20,
+                    radius: 55
+                }
+            ]
+        };
+    }]);
