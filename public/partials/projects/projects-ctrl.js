cb4a62d91b2deaecf20dd2e811fbccefff21279b
rorfan8782 <darcydev1124@gmail.com>
2015-12-04 08:08:49 -0500

    Update project overview page styles

--- public/partials/projects/projects-ctrl.js
+++ public/partials/projects/projects-ctrl.js
@@ -4,5 +4,11 @@
 'use strict'
 
 app.controller('ProjectsCtrl', function($scope, $state, $stateParams) {
+    $scope.schedule_completed = 0;
+    $scope.project_completed = 0;
 
+    $scope.init_overview_page = function() {
+        $scope.schedule_completed = 32;
+        $scope.project_completed = 60;
+    }
 });
