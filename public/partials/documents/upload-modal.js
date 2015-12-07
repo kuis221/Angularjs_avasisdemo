2abebb96dc94cda6893a4e66f4cfa1e7b88e2c43
dreamsoftech <dreamsoftech88@gmail.com>
2015-12-07 10:18:22 -0500

    Finish dropdown feature

--- public/partials/documents/upload-modal.js
+++ public/partials/documents/upload-modal.js
@@ -2,9 +2,52 @@ app.controller('UploadDocumentModalCtrl',
     ['$scope', '$uibModalInstance', function($scope, $uibModalInstance) {
         $scope.dropzoneConfig = {
             url: '/upload',
-            parallelUploads: 3,
-            maxFileSize: 30
+            parallelUploads: 2,
+            maxFileSize: 30,
+            acceptedFiles: "image/*,application/pdf,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/xml",
+            clickable: '.btn-file-input',
+            previewTemplate: $('.upload-preview').html(),
+            previewsContainer: '#upload-files-container',
+            accept: function(file) {
+                var thumbnail = $(file.previewElement).find('span.preview img');
+                switch (file.type) {
+                    case 'application/pdf':
+                        thumbnail.attr('src', "assets/img/documents/icons/pdf_file.png");
+                        // thumbnail.html("<i class='fa fa-file-pdf-o'></i>");
+                        $(file.previewElement).find('span.preview').addClass('not-image');
+                        break;
+                    case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+                        thumbnail.attr('src', "assets/img/documents/icons/text_file.png");
+                        $(file.previewElement).find('span.preview').addClass('not-image');
+                        break;
+                }
+            }
         };
+        $scope.dropzoneEventHandlers = {
+            addedfile: function(file) {
+            },
+            thumbnail: function(file, dataUrl) {
+                console.log(file);
+            },
+        };
+        $scope.folders = [
+            {
+                id: 1,
+                name: "General Documents"
+            }, {
+                id: 2,
+                name: "RFI"
+            }, {
+                id: 3,
+                name: "Change Order"
+            }, {
+                id: 4,
+                name: "Inspections"
+            }, {
+                id: 5,
+                name: "Punch List"
+            }
+        ];
 
         $scope.cancel = function () {
             $uibModalInstance.dismiss('cancel');
