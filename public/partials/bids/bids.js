e5bdec4047dfbb8a4a75222b729bd13ab856ffd1
zonk2world <zonk2world@gmail.com>
2015-12-08 13:26:02 -0800

    all selector work

--- public/partials/bids/bids.js
+++ public/partials/bids/bids.js
@@ -79,7 +79,26 @@ app.controller('BidsCtrl',
                 received: 67,
                 avg_amount: 12435948.00,
                 documents: 2,
-                children: []
+                children: [
+                    {
+                        id: 31,
+                        distrib_to: "Omare Tristique Vestibulum",
+                        company: "ABC Company",
+                        open_status: "ready",
+                        date_submitted: "Aug 23, 2015",
+                        submitted_by: "Mike Mayer",
+                        amount: 879564.5
+                    },
+                    {
+                        id: 32,
+                        distrib_to: "Lorem Dolor",
+                        company: "Flora Corp",
+                        open_status: "ready",
+                        date_submitted: "Aug 23, 2015",
+                        submitted_by: "Jake James",
+                        amount: 1089758.89
+                    }
+                ]
             },
             {
                 id: 4,
@@ -93,15 +112,50 @@ app.controller('BidsCtrl',
                 received: 67,
                 avg_amount: 12435948.00,
                 documents: 2,
-                children: []
+                children: [
+                    {
+                        id: 41,
+                        distrib_to: "Omare Tristique Vestibulum",
+                        company: "ABC Company",
+                        open_status: "ready",
+                        date_submitted: "Aug 23, 2015",
+                        submitted_by: "Mike Mayer",
+                        amount: 879564.5
+                    },
+                    {
+                        id: 42,
+                        distrib_to: "Lorem Dolor",
+                        company: "Flora Corp",
+                        open_status: "ready",
+                        date_submitted: "Aug 23, 2015",
+                        submitted_by: "Jake James",
+                        amount: 1089758.89
+                    }
+                ]
             }
         ];
+        $scope.report = {
+            bids_distrib: 57,
+            completed: 100,
+            response: 68,
+            awarded: 61,
+            amount: 1513986
+        };
+        
         $scope.showBid = [];
         $scope.showDraft = [];
+        $scope.showCreate = [];
+        $scope.showAward = [];        
         $scope.toggleBid = function(index) {
             $scope.showBid[index] = !$scope.showBid[index];
         };
         $scope.toggleDraft = function(index) {
             $scope.showDraft[index] = !$scope.showDraft[index];
+        };
+        $scope.toggleCreate = function(index) {
+            $scope.showCreate[index] = !$scope.showCreate[index];
+        };
+        $scope.toggleAward = function(index) {
+            $scope.showAward[index] = !$scope.showAward[index];
         };
     }]);
