a8006cdd6614399761ce775dd24655e83778ff87
itangel <anton4it@gmail.com>
2015-12-11 20:51:57 +0100

    fixed merge conflict on app.js

--- public/content/js/app.js
+++ public/content/js/app.js
@@ -183,6 +183,36 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
             templateUrl: 'partials/bids/bids.html',
             controller: 'BidsCtrl'
         })
+        .state('procurement', {
+            url: '/procurements',
+            abstract: true,
+            template: '<ui-view></ui-view>'
+        })
+        .state('procurement.main', {
+            url: '/main',
+            templateUrl: 'partials/procurement/list.html',
+            controller: 'ProcurementsCtrl'
+        .state('payment-application', {
+            url: '/financials/payment-application',
+            abstract: true,
+            template: '<ui-view></ui-view>'
+        })
+        .state('payment-application.initiate', {
+            url: '/initiate',
+            templateUrl: "partials/financials/payment-application/payment-initiate.html",
+            controller: "FinancialsPaymentInitiateCtrl",
+            data: {
+                isHeaderHidden: true
+            }
+        })
+        .state('payment-application.finalize', {
+            url: '/finalize',
+            templateUrl: "partials/financials/payment-application/payment-finalize.html",
+            controller: "FinancialsPaymentFinalizeCtrl",
+            data: {
+                isHeaderHidden: true
+            }
+        })
         .state('photos', {
             url: '/photos',
             templateUrl: 'partials/photos/photos.html',
