c4a7b30ea9caa6051efa1528c27bb5d1073f6619
dreamsoftech <dreamsoftech88@gmail.com>
2015-12-12 03:31:53 -0500

    Add schedule stats

--- public/partials/schedules/main.js
+++ public/partials/schedules/main.js
@@ -2,6 +2,12 @@
 
 app.controller('SchedulesCtrl',
     ['$scope', '$uibModal', function($scope, $uibModal) {
-
+        $scope.pieOptions = {
+            type: 'pie',
+            height: '100px',
+            offset: -90,
+            sliceColors: ['#3aabd5', '#f1f1f1', '#f6b875'],
+            disableTooltips: true
+        };
     }]
 );
