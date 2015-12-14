c356286b6ab5c1f25f1c5b218678819f702b3076
zonk2world <zonk2world@gmail.com>
2015-12-14 05:57:42 -0800

    fixed layout -1 in loan info

--- public/partials/financials/loan.js
+++ public/partials/financials/loan.js
@@ -1,7 +1,9 @@
 app.controller('LoanCtrl' ,
-    ['$scope', function ($scope) {
+    ['$scope', '$state', function ($scope, $state) {
+        $scope.$parent.setCurrentTab($state.current.name);
 
         $scope.haveInterest = true;
         $scope.upload = false;
+        $scope.checkCredit = false;
 
     }]);
