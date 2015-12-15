820f18f844eff96ecb0cdd48932ccaf1a1b7b874
zonk2world <zonk2world@gmail.com>
2015-12-14 16:10:52 -0800

    layout for bids compare -4

--- public/partials/bids/bids-compare.js
+++ public/partials/bids/bids-compare.js
@@ -1,4 +1,50 @@
 app.controller('BidsCompareCtrl',
     ['$scope', function($scope){
-
+        $scope.bids = [
+            {
+                id: 1,
+                estimation: "Frusce Risus",
+                duration: 5,
+                material: 1234897,
+                labor: 5987,
+                equipment: 7897,
+                miscell: 300,
+                total: 9234897,
+                addition: "",
+                com_info: true,
+                insure_req: false,
+                history: false,
+                references: true
+            },
+            {
+                id: 2,
+                estimation: "Frusce Risus",
+                duration: 5,
+                material: 1234897,
+                labor: 5987,
+                equipment: 7897,
+                miscell: 300,
+                total: 9234897,
+                addition: "",
+                com_info: true,
+                insure_req: false,
+                history: false,
+                references: true
+            },
+            {
+                id: 3,
+                estimation: "Frusce Risus",
+                duration: 5,
+                material: 1234897,
+                labor: 5987,
+                equipment: 7897,
+                miscell: 300,
+                total: 9234897,
+                addition: "",
+                com_info: true,
+                insure_req: false,
+                history: false,
+                references: true
+            }
+        ];
     }]);
