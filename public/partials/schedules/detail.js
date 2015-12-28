dfa1ccf2113d65ca96ee2f2b09ec0fc34b891bf3
dreamsoftech <dreamsoftech88@gmail.com>
2015-12-28 05:03:44 -0500

    Add comments in schedule detail page

--- public/partials/schedules/detail.js
+++ public/partials/schedules/detail.js
@@ -80,6 +80,31 @@ app.controller('ScheduleDetailCtrl',
             lag: '-'
         }];
 
+        $scope.comments = [{
+            created_by: 'Dave Gebo',
+            avatar: 'assets/img/users/dave_gebo.jpg',
+            created_at: 'August 6, 2015 1:36pm',
+            body: "There is an RFI I'm waiting on that is holding me up. If I get it by the end of today or tomorrow morning, I will get started tomorrow since I missed today."
+        },
+        {
+            created_by: 'Bruce Wayne',
+            avatar: 'assets/img/bruce-wayne.png',
+            created_at: 'August 6, 2015 3:12pm',
+            body: "Hey Dave, thanks for the notice. Working on a Saturday? I like you already... I'll have a few guys out there, ask for help if you need it."
+        },
+        {
+            created_by: 'Dave Gebo',
+            avatar: 'assets/img/users/dave_gebo.jpg',
+            created_at: 'August 13, 2015 4:22pm',
+            body: "Quick update, everything is going smoothly and we are looking ahead of schedule by a couple days."
+        },
+        {
+            created_by: 'Bruce Wayne',
+            avatar: 'assets/img/bruce-wayne.png',
+            created_at: 'August 17, 2015 9:36am',
+            body: "Attention: There will be drying concrete around the project today and tomorrow, please steer clear!"
+        }]
+
         $scope.showHistory = function() {
             var modalInstance = $uibModal.open({
                 templateUrl: 'partials/schedules/details/schedule-history-modal.html',
