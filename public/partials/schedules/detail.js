f5d0875b90310f3f7289ad2996d0a41065bce508
dreamsoftech <dreamsoftech88@gmail.com>
2015-12-28 03:51:53 -0500

    Modify photos layout in schedule detail page

--- public/partials/schedules/detail.js
+++ public/partials/schedules/detail.js
@@ -121,152 +121,18 @@ app.controller('ScheduleDetailCtrl',
         //=============Photos===============
         $scope.folders = [
             {
-                id: 1,
-                name: 'Excavation',
-                date: 'Mar 20, 2015',
-                number_of_photos: 4,
-            }, {
-                id: 2,
-                name: 'Foundation',
-                date: 'Apr 30, 2015',
-                number_of_photos: 9
-            }, {
-                id: 3,
-                name: 'Sub Rough',
-                date: 'Jun 7, 2015',
-                number_of_photos: 4
-            }, {
-                id: 4,
-                name: 'Framing',
-                date: 'Jul 2, 2015',
-                number_of_photos: 6
-            }, {
                 id: 5,
                 name: 'Roofing',
                 date: 'Jul 14, 2015',
                 number_of_photos: 5
-            }, {
-                id: 6,
-                name: 'MEP Rough-In',
-                date: 'Jul 25, 2015',
-                number_of_photos: 4
-            }, {
-                id: 7,
-                name: '4-Way Inspection',
-                date: 'Aug 16, 2015',
-                number_of_photos: 0
             }
         ];
 
         $scope.photos = [
-            // Excavation Folder : 4
-            {
-                id: _.uniqueId(),
-                name: 'photo11.jpg',
-                folders: [1]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo12.jpg',
-                folders: [1]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo13.jpg',
-                folders: [1]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo14.jpg',
-                folders: [1]
-            },
-            // Foundation Folder : 9
-            {
-                id: _.uniqueId(),
-                name: 'photo21.jpg',
-                folders: [2]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo22.jpg',
-                folders: [2]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo23.jpg',
-                folders: [2]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo24.jpg',
-                folders: [2]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo25.jpg',
-                folders: [2]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo26.jpg',
-                folders: [2]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo27.jpg',
-                folders: [2]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo28.jpg',
-                folders: [2]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo29.jpg',
-                folders: [2]
-            },
-            // Sub Rough Folder : 4
-            {
-                id: _.uniqueId(),
-                name: 'photo31.jpg',
-                folders: [3]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo32.jpg',
-                folders: [3]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo33.jpg',
-                folders: [3]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo34.jpg',
-                folders: [3]
-            },
-            // Framing Folder : 6
-            {
-                id: _.uniqueId(),
-                name: 'photo41.jpg',
-                folders: [4]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo42.jpg',
-                folders: [4]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo43.jpg',
-                folders: [4]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo44.jpg',
-                folders: [4]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo45.jpg',
-                folders: [4]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo46.jpg',
-                folders: [4]
-            },
             // Roofing Folder : 5
             {
                 id: _.uniqueId(),
-                name: 'photo51.jpg',
-                folders: [5]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo52.jpg',
+                name: 'photo54.jpg',
                 folders: [5]
             }, {
                 id: _.uniqueId(),
@@ -274,32 +140,14 @@ app.controller('ScheduleDetailCtrl',
                 folders: [5]
             }, {
                 id: _.uniqueId(),
-                name: 'photo54.jpg',
-                folders: [5]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo55.jpg',
+                name: 'photo52.jpg',
                 folders: [5]
             },
-            // MEP Rough-In Folder : 4
             {
                 id: _.uniqueId(),
-                name: 'photo61.jpg',
-                folders: [6]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo62.jpg',
-                folders: [6]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo63.jpg',
-                folders: [6]
-            }, {
-                id: _.uniqueId(),
-                name: 'photo64.jpg',
-                folders: [6]
+                name: 'photo51.jpg',
+                folders: [5]
             }
-            // 4-Way Inspection Folder : 0
         ];
 
         _.each($scope.photos, function(photo) {
